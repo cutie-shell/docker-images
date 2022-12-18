8f1ad330f0153e6e48c8ebaaa3f693d14b830f4c
Erik Inkinen <erik.inkinen@gmail.com>
2022-12-18 18:27:56 +0200

    New S3 repo

---
+++ extra/repo-deploy.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Quick and dirty deployer
+#
+
+if [ "${CIRCLECI}" == "true" ]; then
+	# CircleCI
+
+	BRANCH="${CIRCLE_BRANCH}"
+	COMMIT="${CIRCLE_SHA1}"
+	PROJECT_SLUG="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
+	if [ -n "${CIRCLE_TAG}" ]; then
+		TAG="${CIRCLE_TAG}"
+	fi
+else
+	# Sorry
+	echo "This script runs only on CircleCI!"
+	exit 1
+fi
+
+# Determine target.
+echo "Determining target"
+if [ -n "${TAG}" ]; then
+	# Tag, should go to production
+	TARGET="production"
+elif [[ ${BRANCH} = feature/* ]]; then
+	# Feature branch
+	_project=${PROJECT_SLUG//\//-}
+	_project=${_project//_/-}
+	_branch=${BRANCH/feature\//}
+	_branch=${_branch//./-}
+	_branch=${_branch//_/-}
+	_branch=${_branch//\//-}
+	TARGET=$(echo ${_project}-${_branch} | tr '[:upper:]' '[:lower:]')
+else
+	# Staging
+	TARGET="staging"
+fi
+
+gem install deb-s3
+
+echo -e "${GPG_PACKAGE_SIGNING_KEY}" | gpg --import
+
+echo "Uploading data"
+find /tmp/buildd-results/ \
+	-maxdepth 1 \
+	-regextype posix-egrep \
+	-regex "/tmp/buildd-results/.*\.(u?deb)$" \
+	-print0 \
+	| xargs -0 -i deb-s3 upload {}\
+	--arch="$(dpkg --print-architecture)" \
+	--no-fail-if-exists \
+	--bucket=deb.cutie-shell.org \
+	--prefix=${TARGET} \
+	--codename=bookworm \
+	--access-key-id=${AWS_ACCESS_KEY_ID} \
+	--secret-access-key=${AWS_SECRET_ACCESS_KEY} \
+	--s3-region=${AWS_DEFAULT_REGION} \
+	--sign=8BA0C00869CDCD4D
+
--- extra/repo-droidian-sign.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Quick and dirty signer
-#
-
-set -e
-
-echo "${GPG_STAGINGPRODUCTION_SIGNING_KEY}" | gpg --import
-exec debsign -k6A546270CDFD4BB5 *.changes
